45f9595f7c7afc5cf3dacf5d1d9b6e4ee31ea0d6
Mukeshraykumar1 <mukeshraykumar19101995@gmail.com>
2023-05-07 12:37:12 +0530

    header

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+import Header from "./Layout/Header";
+function App() {
+  return (
+    <Fragment>
+      <header />
+    </Fragment>
+  );
+}
+
+export default App;
