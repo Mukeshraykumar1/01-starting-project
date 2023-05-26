a07a2619fd339d63c0663a1ffc99c0094f3dd7e2
Mukeshraykumar1 <mukeshraykumar19101995@gmail.com.com>
2023-05-26 12:42:16 +0530

    cart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ProductList from "./Product/ProductList";
+import Cart from "./Cart/Cart";
 
 function App() {
   return (
     <div>
       <ProductList />
+      <Cart/>
     </div>
   );
 }
