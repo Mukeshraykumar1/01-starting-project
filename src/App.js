a81cc06f05b1557ee5e2a5c79aa7ebccedd70610
Mukeshraykumar1 <mukeshraykumar19101995@gmail.com.com>
2023-05-26 12:07:26 +0530

    on-screen

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import { useState } from "react";
-import Header from "./Layout/Header";
-import Meals from "./Meals/Meals";
-import Cart from "./Cart/Cart";
-import CartProvider from "./store/CartProvider";
+import React from "react";
+import ProductList from "./Product/ProductList";
+
 function App() {
-const [cartIsShown, setCartIsShown] = useState(false);
-
-const showCartHandler = () => {
-  setCartIsShown(true);
-}
-
-const hideCartHandler = () => {
-   setCartIsShown(false);
-}
-
   return (
-    <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-      <main>
-      <Meals />
-      </main>
-    </CartProvider>
+    <div>
+      <ProductList />
+    </div>
   );
 }
 
